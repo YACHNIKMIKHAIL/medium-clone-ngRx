86f90aa81488e98c9b3b689b3eb01c250f704bbc
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-01-24 16:01:00 +0100

    bad commit

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Store } from '@ngrx/store';
+import { registerAction } from '../store/register.actions';
 
 @Component({
       selector: 'mc-register',
@@ -9,7 +11,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class RegisterComponent implements OnInit {
       public form: FormGroup = new FormGroup<any>({});
 
-      constructor(private fb: FormBuilder) {}
+      constructor(private fb: FormBuilder, private store: Store) {}
 
       ngOnInit(): void {
             this.initializeForm();
@@ -29,5 +31,6 @@ export class RegisterComponent implements OnInit {
       onSubmit(): void {
             console.log(this.form.valid);
             console.log(this.form.value);
+            this.store.dispatch(registerAction(this.form.value));
       }
 }
