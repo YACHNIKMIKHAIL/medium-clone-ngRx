82f390ed9bc840540e0c1f3004b1159ed1cb5414
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-01-26 22:55:13 +0100

    add TopBarComponent

---
+++ src/app/shared/modules/top-bar/components/top-bar.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+      selector: 'mc-top-bar',
+      templateUrl: './top-bar.component.html',
+      styleUrls: ['./top-bar.component.scss'],
+})
+export class TopBarComponent implements OnInit {
+      constructor() {}
+
+      ngOnInit(): void {}
+}
