5b77391fe79748ace8db53d4b35f9be132236d66
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-01-26 23:06:42 +0100

    add TopBarModule

--- src/app/shared/modules/top-bar/top-bar.module.ts
+++ src/app/shared/modules/top-bar/top-bar.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TopBarComponent } from './components/top-bar.component';
+import { RouterLinkWithHref } from '@angular/router';
 
 @NgModule({
       declarations: [TopBarComponent],
-      imports: [CommonModule],
+      imports: [CommonModule, RouterLinkWithHref],
+      exports: [TopBarComponent],
 })
 export class TopBarModule {}
