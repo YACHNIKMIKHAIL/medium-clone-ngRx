90cd5a57aa494f0340b613e90fb11a86be5eb8d1
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-01-26 21:44:05 +0100

    update AuthService

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -5,17 +5,29 @@ import { map, Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../../environments/environment.prod';
 import { AuthResponseInterface } from '../types/authResponse.interface';
+import { LoginRequestInterface } from '../types/loginRequest.interface';
 
 @Injectable()
 export class AuthService {
+      private url: string = environment.apiUrl;
       constructor(private http: HttpClient) {}
       register(
             data: RegisterRequestInterface,
       ): Observable<CurrentUserInterface> {
-            console.log('register', data);
-            const url = environment.apiUrl + '/users';
             return this.http
-                  .post<AuthResponseInterface>(url, data)
-                  .pipe(map(response => response.user));
+                  .post<AuthResponseInterface>(this.url + '/users', data)
+                  .pipe(this.getUser);
+      }
+
+      login(data: LoginRequestInterface): Observable<CurrentUserInterface> {
+            return this.http
+                  .post<AuthResponseInterface>(this.url + '/users/login', data)
+                  .pipe(this.getUser);
+      }
+
+      private getUser(
+            source$: Observable<AuthResponseInterface>,
+      ): Observable<CurrentUserInterface> {
+            return source$.pipe(map(response => response.user));
       }
 }
