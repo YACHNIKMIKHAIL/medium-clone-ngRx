51be17141c35380eb4256e66b059f8372f48ee8a
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-01-30 21:54:44 +0100

    animate loading comp

--- src/app/shared/modules/loading/components/loading.component.ts
+++ src/app/shared/modules/loading/components/loading.component.ts
@@ -1,10 +1,25 @@
 import { Component } from "@angular/core";
+import { interval, map, repeat, take } from "rxjs";
 
 @Component({
       selector: "mc-loading",
       template:
-            "<div class='wrap'> <img src=\"https://www.wall-art.de/out/pictures/generated/product/2/780_780_80/2715-wandtattoo-ladebalken-web-einzel.jpg\" \n" +
-            '     alt="loading"></div>',
-      styles: [".wrap{margin:0 auto;}", "img{width:200px;}"],
+            '<ng-container *ngIf="(counter$ | async) as progress">' +
+            '<div class="progress-bar" [style.width.%]="progress"></div>' +
+            "</ng-container>",
+      styles: [
+            ".progress-bar {\n" +
+                  "  height: 1rem;\n" +
+                  "  background-color: #5cb85c;\n" +
+                  "  margin-top: 20px;\n" +
+                  "}",
+      ],
 })
-export class LoadingComponent {}
+export class LoadingComponent {
+      source = interval(15);
+      counter$ = this.source.pipe(
+            take(100),
+            map(value => value + 1),
+            repeat(5),
+      );
+}
