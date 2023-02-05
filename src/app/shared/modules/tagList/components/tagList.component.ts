6fd14994637ff8964d903e3003aec7eacc607ee8
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-05 18:51:29 +0100

    some ref of mc-tagList comp

--- src/app/shared/modules/tagList/components/tagList.component.ts
+++ src/app/shared/modules/tagList/components/tagList.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input } from "@angular/core";
+import { PopularTagType } from "../../../types/popularTagType";
 
 @Component({
-      selector: "mc-tagList",
+      selector: "mc-tag-list",
       templateUrl: "./tagList.component.html",
       styleUrls: ["./tagList.component.scss"],
 })
 export class TagListComponent {
-      @Input("tagList") tagListProps: string[] | null = null;
+      @Input("tagList") tagListProps: PopularTagType[] | null = null;
 }
