13707ae90aacaec68ec2037d0c7151f26530554d
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-06 13:32:15 +0100

    add routerLinkActive

--- src/app/shared/modules/toggle-feed/feed-toggle.module.ts
+++ src/app/shared/modules/toggle-feed/feed-toggle.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { FeedToggleComponent } from "./components/feed-toggle.component";
+import { RouterModule } from "@angular/router";
 
 @NgModule({
       declarations: [FeedToggleComponent],
       exports: [FeedToggleComponent],
-      imports: [CommonModule],
+      imports: [CommonModule, RouterModule],
 })
 export class FeedToggleModule {}
