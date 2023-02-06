c9ce14f19f4d31b39bcbd40809ea5a47213a0b12
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-06 13:38:20 +0100

    finish feed-toggle

--- src/app/global-feed/components/global-feed/global-feed.component.ts
+++ src/app/global-feed/components/global-feed/global-feed.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from "@angular/core";
 import { select, Store } from "@ngrx/store";
 import { isLoggedInSelector } from "../../../auth/store/selectors";
 import { Observable } from "rxjs";
-import { feedSlugSelector } from "../../../shared/modules/feed/store/selectors";
 
 @Component({
       selector: "mc-global-feed",
@@ -12,12 +11,10 @@ import { feedSlugSelector } from "../../../shared/modules/feed/store/selectors";
 export class GlobalFeedComponent implements OnInit {
       public url = "/articles"; //"/articles/feed?limit=10&off"
       public isLoggedIn$ = new Observable<boolean | null>();
-      public feedSlug$ = new Observable<string | null>();
 
       constructor(private store: Store) {}
 
       ngOnInit(): void {
             this.isLoggedIn$ = this.store.pipe(select(isLoggedInSelector));
-            this.feedSlug$ = this.store.pipe(select(feedSlugSelector));
       }
 }
