16f8767304980bbd798579fd93ee537c97afc8d4
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-07 17:28:55 +0100

    finish article component

--- src/app/article/components/article.component.ts
+++ src/app/article/components/article.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnDestroy, OnInit } from "@angular/core";
-import { Observable, Subscription } from "rxjs";
+import { combineLatest, map, Observable, Subscription } from "rxjs";
 import { ArticleInterface } from "../../shared/types/article.interface";
 import { select, Store } from "@ngrx/store";
 import {
@@ -9,6 +9,7 @@ import {
 } from "../store/selectors";
 import { getArticleAction } from "../store/actions/get-article.actions";
 import { ActivatedRoute } from "@angular/router";
+import { currentUserSelector } from "../../auth/store/selectors";
 
 @Component({
       selector: "mc-article",
@@ -18,9 +19,11 @@ import { ActivatedRoute } from "@angular/router";
 export class ArticleComponent implements OnInit, OnDestroy {
       public article: ArticleInterface | null = null;
       public articleSubscription = new Subscription();
+      // public isAuthorSubscription = new Subscription();
       public isLoading$ = new Observable<boolean>();
       public error$ = new Observable<string | null>();
       public slug: string | null = null;
+      public isAuthor$ = new Observable<boolean>();
       constructor(private store: Store, private route: ActivatedRoute) {}
       ngOnInit(): void {
             this.initializeValues();
@@ -35,6 +38,17 @@ export class ArticleComponent implements OnInit, OnDestroy {
             this.error$ = this.store.pipe(select(articleErrorSelector));
 
             this.slug = this.route.snapshot.paramMap.get("slug");
+
+            this.isAuthor$ = combineLatest(
+                  this.store.pipe(select(currentUserSelector)),
+                  this.store.pipe(select(articleSelector)),
+            ).pipe(
+                  map(
+                        ([currentUser, article]) =>
+                              currentUser?.username ===
+                              article?.author.username,
+                  ),
+            );
       }
 
       fetchArticle(): void {
@@ -44,5 +58,6 @@ export class ArticleComponent implements OnInit, OnDestroy {
 
       ngOnDestroy(): void {
             this.articleSubscription.unsubscribe();
+            // this.isAuthorSubscription.unsubscribe();
       }
 }
