47638982cfa339c135458cd1dfd2427175ae708d
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-07 13:37:04 +0100

    conflict 2 conflict 2

--- src/app/shared/types/appState.interface.ts
+++ src/app/shared/types/appState.interface.ts
@@ -3,7 +3,7 @@ import { FeedStateInterface } from "../modules/feed/types/feed-state.interface";
 import { PopularTagsStateInterface } from "../modules/popular-tags/types/popular-tags-state.interface";
 
 export interface AppStateInterface {
-      auth: AuthStateInterface; //bla 1
+      auth: AuthStateInterface; //conflict 2
       feed: FeedStateInterface;
       popularTags: PopularTagsStateInterface;
 }
