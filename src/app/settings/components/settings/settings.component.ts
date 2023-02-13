a70d2353b487e6cc02a1110e0e208b25e13ba060
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-13 13:43:09 +0100

    add CurrentUserForm

--- src/app/settings/components/settings/settings.component.ts
+++ src/app/settings/components/settings/settings.component.ts
@@ -1,12 +1,68 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import {
+      FormBuilder,
+      FormControl,
+      FormGroup,
+      Validators,
+} from "@angular/forms";
+import { select, Store } from "@ngrx/store";
+import { CurrentUserInterface } from "../../../shared/types/current-user.interface";
+import { filter, Subscription } from "rxjs";
+import { currentUserSelector } from "../../../auth/store/selectors";
 
+interface CurrentUserFormInterface {
+      image: FormControl<string | null | undefined>;
+      username: FormControl<string | null | undefined>;
+      bio: FormControl<string | null | undefined>;
+      email: FormControl<string | null | undefined>;
+      password: FormControl<string | null>;
+}
 @Component({
       selector: "mc-settings",
       templateUrl: "./settings.component.html",
       styleUrls: ["./settings.component.scss"],
 })
-export class SettingsComponent implements OnInit {
-      constructor() {}
+export class SettingsComponent implements OnInit, OnDestroy {
+      currentUser: CurrentUserInterface | null = null;
+      currentUserSubscription = new Subscription();
+      currentUserForm: FormGroup<CurrentUserFormInterface> = new FormGroup(
+            {} as CurrentUserFormInterface,
+      );
+      constructor(private fb: FormBuilder, private store: Store) {}
 
-      ngOnInit(): void {}
+      ngOnInit(): void {
+            this.initializeListeners();
+      }
+
+      ngOnDestroy(): void {
+            this.currentUserSubscription.unsubscribe();
+      }
+
+      initializeListeners(): void {
+            this.currentUserSubscription = this.store
+                  .pipe(select(currentUserSelector), filter(Boolean))
+                  .subscribe(currentUser => {
+                        this.currentUser = currentUser;
+                        this.initializeForm();
+                  });
+      }
+      initializeForm(): void {
+            this.currentUserForm = this.fb.group({
+                  image: new FormControl(this.currentUser?.image, [
+                        Validators.required,
+                  ]),
+                  username: new FormControl(this.currentUser?.username, [
+                        Validators.required,
+                  ]),
+                  bio: new FormControl(this.currentUser?.bio),
+                  email: new FormControl(this.currentUser?.email, [
+                        Validators.required,
+                  ]),
+                  password: new FormControl("", [Validators.required]),
+            });
+      }
+
+      updateUser() {
+            console.log(this.currentUserForm.value);
+      }
 }
--- src/app/settings/settings.module.ts
+++ src/app/settings/settings.module.ts
@@ -4,6 +4,8 @@ import { SettingsComponent } from "./components/settings/settings.component";
 import { RouterModule, Routes } from "@angular/router";
 import { StoreModule } from "@ngrx/store";
 import { settingsReducer } from "./store/redusers";
+import { ReactiveFormsModule } from "@angular/forms";
+import { LoadingModule } from "../shared/modules/loading/loading.module";
 
 const routes: Routes = [
       {
@@ -17,6 +19,8 @@ const routes: Routes = [
             CommonModule,
             RouterModule.forChild(routes),
             StoreModule.forFeature("settings", settingsReducer),
+            ReactiveFormsModule,
+            LoadingModule,
       ],
 })
 export class SettingsModule {}
