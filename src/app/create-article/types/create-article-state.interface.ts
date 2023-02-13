13276b41054d7ec1eeb63b828d53419af2033912
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-13 09:20:15 +0100

    fix import interface

--- src/app/create-article/types/create-article-state.interface.ts
+++ src/app/create-article/types/create-article-state.interface.ts
@@ -1,5 +1,4 @@
 import { BackendErrorsInterface } from "../../shared/types/backend-errors.interface";
-import { BackendErrorsInterface } from "../../shared/types/backendErrors.interface";
 
 export interface CreateArticleStateInterface {
       isSubmitting: boolean;
