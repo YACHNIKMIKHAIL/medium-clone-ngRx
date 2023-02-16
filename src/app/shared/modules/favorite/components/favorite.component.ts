41faa0f6b8523e5c7b482086bbf9831864329a77
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-16 12:55:47 +0100

    realize navigate

--- src/app/shared/modules/favorite/components/favorite.component.ts
+++ src/app/shared/modules/favorite/components/favorite.component.ts
@@ -1,34 +1,47 @@
-import { Component, Input, OnInit } from "@angular/core";
-import { Observable } from "rxjs";
+import { Component, Input, OnDestroy, OnInit } from "@angular/core";
+import { Observable, Subscription } from "rxjs";
 import { select, Store } from "@ngrx/store";
 import { disabledSlugSelector, isLoadingSelector } from "../store/selectors";
 import {
       dislikeFavoriteAction,
       favoriteAction,
 } from "../store/actions/favorite.actions";
+import { isLoggedInSelector } from "../../../../auth/store/selectors";
+import { Router } from "@angular/router";
 
 @Component({
       selector: "mc-favorite",
       templateUrl: "./favorite.component.html",
       styleUrls: ["./favorite.component.scss"],
 })
-export class FavoriteComponent implements OnInit {
+export class FavoriteComponent implements OnInit, OnDestroy {
       @Input("favoritesCount") favoritesCountProps!: number;
       @Input("isFavorite") isFavoriteProps!: boolean;
       @Input("slug") slugProps!: string;
       isLoading$ = new Observable<boolean>();
+      isLoggedIn: boolean | null = null;
       disabledSlug$ = new Observable<string | null>();
-      constructor(private store: Store) {}
+      isLoggedInSub = new Subscription();
+      constructor(private store: Store, private router: Router) {}
 
       ngOnInit(): void {
             this.isLoading$ = this.store.pipe(select(isLoadingSelector));
+            this.isLoggedInSub = this.store
+                  .pipe(select(isLoggedInSelector))
+                  .subscribe(log => (this.isLoggedIn = log));
             this.disabledSlug$ = this.store.pipe(select(disabledSlugSelector));
       }
 
       likeOrDislike() {
             const slug = this.slugProps;
-            this.isFavoriteProps
+            !this.isLoggedIn
+                  ? this.router.navigate(["/login"])
+                  : this.isFavoriteProps
                   ? this.store.dispatch(dislikeFavoriteAction({ slug }))
                   : this.store.dispatch(favoriteAction({ slug }));
+      }
+
+      ngOnDestroy(): void {
+            this.isLoggedInSub.unsubscribe();
       }
 }
