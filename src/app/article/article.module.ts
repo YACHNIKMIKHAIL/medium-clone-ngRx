a75d76ff69ff7777f4cb4ede304b845ff8ecd7ca
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-16 13:58:21 +0100

    realize add to favorites comp

--- src/app/article/article.module.ts
+++ src/app/article/article.module.ts
@@ -10,6 +10,7 @@ import { ErrorMessageModule } from "../shared/modules/error-message/error-messag
 import { LoadingModule } from "../shared/modules/loading/loading.module";
 import { TagListModule } from "../shared/modules/tagList/tagList.module";
 import { DeleteArticleEffect } from "./store/effects/delete-article.effect";
+import { AddToFavoritesModule } from "../shared/modules/add-to-favorites/add-to-favorites.module";
 
 const routes: Routes = [
       {
@@ -28,6 +29,7 @@ const routes: Routes = [
             LoadingModule,
             RouterModule.forChild(routes),
             TagListModule,
+            AddToFavoritesModule,
       ],
 })
 export class ArticleModule {}
