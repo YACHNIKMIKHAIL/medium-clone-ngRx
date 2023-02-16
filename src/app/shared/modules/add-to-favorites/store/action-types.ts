f306efadbde93fb3b432d71b45ea014f37d446b3
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-16 16:59:10 +0100

    realize effect

--- src/app/shared/modules/add-to-favorites/store/action-types.ts
+++ src/app/shared/modules/add-to-favorites/store/action-types.ts
@@ -1,4 +1,8 @@
 export enum ActionTypes {
-      ADD_TO_FAVORITES = "[Like] Add to favorites",
-      REMOVE_FROM_FAVORITES = "[Like] Remove from favorites",
+      ADD_TO_FAVORITES = "[Add to favorites] Add to favorites",
+      ADD_TO_FAVORITES_SUCCESS = "[Add to favorites] Add to favorites success",
+      ADD_TO_FAVORITES_FAILURE = "[Add to favorites] Add to favorites failure",
+      REMOVE_FROM_FAVORITES = "[Add to favorites] Remove from favorites",
+      REMOVE_FROM_FAVORITES_SUCCESS = "[Add to favorites] Remove from favorites success",
+      REMOVE_FROM_FAVORITES_FAILURE = "[Add to favorites] Remove from favorites failure",
 }
