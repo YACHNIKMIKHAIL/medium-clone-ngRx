28b1f603353d14265543c1cb9350c6aa560bb1f0
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-16 17:10:26 +0100

    update effect

--- src/app/shared/modules/add-to-favorites/store/actions/add-to-favorites.actions.ts
+++ src/app/shared/modules/add-to-favorites/store/actions/add-to-favorites.actions.ts
@@ -4,7 +4,7 @@ import { ArticleInterface } from "../../../../types/article.interface";
 
 export const addToFavoritesAction = createAction(
       ActionTypes.ADD_TO_FAVORITES,
-      props<{ slug: string }>(),
+      props<{ isFavorited: boolean; slug: string }>(),
 );
 export const addToFavoritesSuccessAction = createAction(
       ActionTypes.ADD_TO_FAVORITES_SUCCESS,
@@ -14,15 +14,15 @@ export const addToFavoritesFailureAction = createAction(
       ActionTypes.ADD_TO_FAVORITES_FAILURE,
 );
 
-export const removeFromFavoritesAction = createAction(
-      ActionTypes.REMOVE_FROM_FAVORITES,
-      props<{ slug: string }>(),
-);
+// export const removeFromFavoritesAction = createAction(
+//       ActionTypes.REMOVE_FROM_FAVORITES,
+//       props<{ slug: string }>(),
+// );
 
-export const removeFromFavoritesSuccessAction = createAction(
-      ActionTypes.REMOVE_FROM_FAVORITES_SUCCESS,
-      props<{ article: ArticleInterface }>(),
-);
-export const removeFromFavoritesFailureAction = createAction(
-      ActionTypes.REMOVE_FROM_FAVORITES_FAILURE,
-);
+// export const removeFromFavoritesSuccessAction = createAction(
+//       ActionTypes.REMOVE_FROM_FAVORITES_SUCCESS,
+//       props<{ article: ArticleInterface }>(),
+// );
+// export const removeFromFavoritesFailureAction = createAction(
+//       ActionTypes.REMOVE_FROM_FAVORITES_FAILURE,
+// );
--- src/app/shared/modules/add-to-favorites/store/effects/add-to-favorites.effect.ts
+++ src/app/shared/modules/add-to-favorites/store/effects/add-to-favorites.effect.ts
@@ -1,61 +1,62 @@
 import { Actions, createEffect, ofType } from "@ngrx/effects";
-import { catchError, map, of, switchMap } from "rxjs";
+import { catchError, map, Observable, of, switchMap } from "rxjs";
 import { Injectable } from "@angular/core";
 import {
       addToFavoritesAction,
       addToFavoritesFailureAction,
       addToFavoritesSuccessAction,
-      removeFromFavoritesAction,
-      removeFromFavoritesFailureAction,
-      removeFromFavoritesSuccessAction,
 } from "../actions/add-to-favorites.actions";
 import { AddToFavoritesService } from "../../services/add-to-favorites.service";
 import { ArticleInterface } from "../../../../types/article.interface";
 
 @Injectable()
 export class AddToFavoritesEffect {
+      private defineNeededMethod(
+            isFavorited: boolean,
+            slug: string,
+      ): Observable<ArticleInterface> {
+            return isFavorited
+                  ? this.addToFavoritesService.removeArticleFromFavorite(slug)
+                  : this.addToFavoritesService.addArticleToFavorite(slug);
+      }
       addToFavorites$ = createEffect(() =>
             this.actions$.pipe(
                   ofType(addToFavoritesAction),
-                  switchMap(({ slug }) => {
-                        return this.addToFavoritesService
-                              .addArticleToFavorite(slug)
-                              .pipe(
-                                    map((article: ArticleInterface) => {
-                                          return addToFavoritesSuccessAction({
-                                                article,
-                                          });
-                                    }),
-                                    catchError(() => {
-                                          return of(
-                                                addToFavoritesFailureAction(),
-                                          );
-                                    }),
-                              );
+                  switchMap(({ isFavorited, slug }) => {
+                        return this.defineNeededMethod(isFavorited, slug).pipe(
+                              map((article: ArticleInterface) => {
+                                    return addToFavoritesSuccessAction({
+                                          article,
+                                    });
+                              }),
+                              catchError(() => {
+                                    return of(addToFavoritesFailureAction());
+                              }),
+                        );
                   }),
             ),
       );
-      removeFromFavorites$ = createEffect(() =>
-            this.actions$.pipe(
-                  ofType(removeFromFavoritesAction),
-                  switchMap(({ slug }) => {
-                        return this.addToFavoritesService
-                              .removeArticleFromFavorite(slug)
-                              .pipe(
-                                    map((article: ArticleInterface) => {
-                                          return removeFromFavoritesSuccessAction(
-                                                { article },
-                                          );
-                                    }),
-                                    catchError(() => {
-                                          return of(
-                                                removeFromFavoritesFailureAction(),
-                                          );
-                                    }),
-                              );
-                  }),
-            ),
-      );
+      // removeFromFavorites$ = createEffect(() =>
+      //       this.actions$.pipe(
+      //             ofType(removeFromFavoritesAction),
+      //             switchMap(({ slug }) => {
+      //                   return this.addToFavoritesService
+      //                         .removeArticleFromFavorite(slug)
+      //                         .pipe(
+      //                               map((article: ArticleInterface) => {
+      //                                     return removeFromFavoritesSuccessAction(
+      //                                           { article },
+      //                                     );
+      //                               }),
+      //                               catchError(() => {
+      //                                     return of(
+      //                                           removeFromFavoritesFailureAction(),
+      //                                     );
+      //                               }),
+      //                         );
+      //             }),
+      //       ),
+      // );
 
       constructor(
             private actions$: Actions,
