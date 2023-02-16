5e1b64f5a79310052892abffd038ba9ff9ff8348
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-16 14:29:49 +0100

    add input values to comp

--- src/app/shared/modules/add-to-favorites/components/add-to-favorites.component.ts
+++ src/app/shared/modules/add-to-favorites/components/add-to-favorites.component.ts
@@ -10,8 +10,29 @@ export class AddToFavoritesComponent implements OnInit {
       @Input("articleSlug") articleSlugProps!: string;
       @Input("favoritesCount") favoritesCountProps!: number;
       @Input("text") textProps?: string;
+      isFavorited: boolean | unknown;
+      articleSlug: string | unknown;
+      favoritesCount: number | unknown;
+      text: string | unknown;
 
       constructor() {}
 
-      ngOnInit(): void {}
+      ngOnInit(): void {
+            this.initValues();
+      }
+
+      handleLike() {
+            if (this.isFavoritedProps) {
+                  console.log("dislike -");
+            } else {
+                  console.log("like +");
+            }
+      }
+
+      initValues(): void {
+            this.isFavorited = this.isFavoritedProps;
+            this.articleSlug = this.articleSlugProps;
+            this.favoritesCount = this.favoritesCountProps;
+            this.text = this.textProps;
+      }
 }
