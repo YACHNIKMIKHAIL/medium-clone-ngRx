3c2269df0d87c28abd724d9850e761ff7f51b694
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-16 17:23:34 +0100

    clear code

--- src/app/shared/modules/add-to-favorites/store/action-types.ts
+++ src/app/shared/modules/add-to-favorites/store/action-types.ts
@@ -2,7 +2,4 @@ export enum ActionTypes {
       ADD_TO_FAVORITES = "[Add to favorites] Add to favorites",
       ADD_TO_FAVORITES_SUCCESS = "[Add to favorites] Add to favorites success",
       ADD_TO_FAVORITES_FAILURE = "[Add to favorites] Add to favorites failure",
-      // REMOVE_FROM_FAVORITES = "[Add to favorites] Remove from favorites",
-      // REMOVE_FROM_FAVORITES_SUCCESS = "[Add to favorites] Remove from favorites success",
-      // REMOVE_FROM_FAVORITES_FAILURE = "[Add to favorites] Remove from favorites failure",
 }
