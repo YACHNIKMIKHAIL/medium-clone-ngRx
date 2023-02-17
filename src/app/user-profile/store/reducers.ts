1f768e3bfad049e3d81798ff7221ba1b471935d0
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-17 10:44:07 +0100

    add actions & effects

---
+++ src/app/user-profile/store/reducers.ts
@@ -0,0 +1,103 @@
+import { AuthStateInterface } from "../types/authState.interface";
+import { Action, createReducer, on } from "@ngrx/store";
+import {
+      registerAction,
+      registerFailureAction,
+      registerSuccessAction,
+} from "./actions/register.actions";
+import {
+      loginAction,
+      loginFailureAction,
+      loginSuccessAction,
+} from "./actions/login.action";
+import {
+      getCurrentUserAction,
+      getCurrentUserFailureAction,
+      getCurrentUserSuccessAction,
+} from "./actions/get-user-profile.actions";
+import { updateCurrentUserSuccessAction } from "./actions/update-current-user.actions";
+import { logoutAction } from "./actions/sync.actions";
+
+const initialState: AuthStateInterface = {
+      isSubmitting: false,
+      currentUser: null,
+      isLoggedIn: null,
+      validationErrors: null,
+      isLoading: false,
+};
+
+export const authReducer = createReducer(
+      initialState,
+      on(
+            registerAction,
+            (state): AuthStateInterface => ({
+                  ...state,
+                  isSubmitting: true,
+                  validationErrors: null,
+            }),
+      ),
+      on(
+            registerSuccessAction,
+            (state, action): AuthStateInterface => ({
+                  ...state,
+                  isSubmitting: false,
+                  currentUser: action.currentUser,
+                  isLoggedIn: true,
+            }),
+      ),
+      on(
+            registerFailureAction,
+            (state, action): AuthStateInterface => ({
+                  ...state,
+                  isSubmitting: false,
+                  validationErrors: action.errors,
+            }),
+      ),
+      on(loginAction, (state: AuthStateInterface) => ({
+            ...state,
+            isSubmitting: true,
+            validationErrors: null,
+      })),
+      on(
+            loginSuccessAction,
+            (state, action): AuthStateInterface => ({
+                  ...state,
+                  isSubmitting: false,
+                  currentUser: action.currentUser,
+                  isLoggedIn: true,
+            }),
+      ),
+      on(loginFailureAction, (state, action) => ({
+            ...state,
+            isSubmitting: false,
+            validationErrors: action.errors,
+      })),
+      on(getCurrentUserAction, state => ({
+            ...state,
+            isLoading: true,
+      })),
+      on(getCurrentUserSuccessAction, (state, action) => ({
+            ...state,
+            isLoggedIn: true,
+            currentUser: action.currentUser,
+            isLoading: false,
+      })),
+      on(getCurrentUserFailureAction, state => ({
+            ...state,
+            isLoggedIn: false,
+            currentUser: null,
+            isLoading: false,
+      })),
+      on(updateCurrentUserSuccessAction, (state, action) => ({
+            ...state,
+            currentUser: action.currentUserInput,
+      })),
+      on(logoutAction, () => ({ ...initialState, isLoggedIn: false })),
+);
+
+export function reducer(
+      state: AuthStateInterface,
+      action: Action,
+): AuthStateInterface {
+      return authReducer(state, action);
+}
