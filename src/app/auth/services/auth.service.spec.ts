16c51400158045f3c2dd14ac5852418109b99c1b
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-20 17:01:38 +0100

    broken flow

--- src/app/auth/services/auth.service.spec.ts
+++ src/app/auth/services/auth.service.spec.ts
@@ -1,10 +1,15 @@
 import { TestBed } from "@angular/core/testing";
 
 import { AuthService } from "./auth.service";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import {
+      HttpClientTestingModule,
+      HttpTestingController,
+} from "@angular/common/http/testing";
+import { environment } from "../../../environments/environment.prod";
 
 describe("AuthService", () => {
       let service: AuthService;
+      let httpTestingController: HttpTestingController;
 
       beforeEach(() => {
             TestBed.configureTestingModule({
@@ -12,9 +17,53 @@ describe("AuthService", () => {
                   providers: [AuthService],
             });
             service = TestBed.inject(AuthService);
+            httpTestingController = TestBed.inject(HttpTestingController);
       });
 
       it("should be created", () => {
-            expect(service).toBeTruthy();
+            const data = {
+                  user: {
+                        username: "string",
+                        email: "string2",
+                        password: "string44",
+                  },
+            };
+            service.register(data).subscribe(resp => {
+                  expect(resp).toBeTruthy();
+                  expect(resp.email).toBe("string1");
+
+                  const req = httpTestingController.expectOne(
+                        `${environment.apiUrl}/usersss`,
+                  );
+                  expect(req.request.method).toBe("POST");
+                  expect(req.request.body.user.username).toBe(
+                        "data.user.username",
+                  );
+
+                  req.flush({
+                        user: {
+                              bio: "string",
+                              email: "string3",
+                              image: "string",
+                              token: "string",
+                              username: "string",
+                        },
+                  });
+            });
+      });
+      it("should register", () => {
+            pending();
+      });
+      it("should login", () => {
+            pending();
+      });
+      it("should getCurrentUser", () => {
+            pending();
+      });
+      it("should updateCurrentUser", () => {
+            pending();
+      });
+      it("should logoutCurrentUser", () => {
+            pending();
       });
 });
