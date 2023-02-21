65b342a861b22b3f5f57c20d0e4f008c41d2f2e9
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-21 10:36:34 +0100

    UtilsService was covered

--- src/app/shared/services/utils.service.spec.ts
+++ src/app/shared/services/utils.service.spec.ts
@@ -13,4 +13,14 @@ describe("UtilsService", () => {
       it("should be created", () => {
             expect(service).toBeTruthy();
       });
+
+      it("range", () => {
+            const result1 = service.range(2, 6);
+            const result2 = service.range(5, 10);
+            const result3 = service.range(1, 2);
+
+            expect(result1).toEqual([2, 3, 4, 5, 6, 7]);
+            expect(result2).toEqual([5, 6, 7, 8, 9, 10, 11, 12, 13, 14]);
+            expect(result3).toEqual([1, 2]);
+      });
 });
