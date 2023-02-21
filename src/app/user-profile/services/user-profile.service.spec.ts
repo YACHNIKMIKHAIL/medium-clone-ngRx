f585e75195252d917d86c93fc45e2a5510024f8f
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-21 10:31:58 +0100

    UserProfileService was covered

--- src/app/user-profile/services/user-profile.service.spec.ts
+++ src/app/user-profile/services/user-profile.service.spec.ts
@@ -1,19 +1,46 @@
 import { TestBed } from "@angular/core/testing";
 
 import { UserProfileService } from "./user-profile.service";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import {
+      HttpClientTestingModule,
+      HttpTestingController,
+} from "@angular/common/http/testing";
+import { environment } from "../../../environments/environment";
 
 describe("UserProfileService", () => {
       let service: UserProfileService;
+      let httpTestingController: HttpTestingController;
 
       beforeEach(() => {
             TestBed.configureTestingModule({
                   imports: [HttpClientTestingModule],
             });
             service = TestBed.inject(UserProfileService);
+            httpTestingController = TestBed.get(HttpTestingController);
       });
 
       it("should be created", () => {
             expect(service).toBeTruthy();
+      });
+
+      it("should getUserProfile", () => {
+            service.getUserProfile("requestDataSlug").subscribe(resp => {
+                  expect(resp).toBeTruthy();
+                  expect(resp.image).toBe("profile /image");
+                  expect(resp.username).toBe("profile / name / abc");
+            });
+            const req = httpTestingController.expectOne(
+                  `${environment.apiUrl}/profiles/requestDataSlug`,
+            );
+            expect(req.request.method).toBe("GET");
+
+            req.flush({
+                  profile: {
+                        bio: null,
+                        following: true,
+                        image: "profile /image",
+                        username: "profile / name / abc",
+                  },
+            });
       });
 });
