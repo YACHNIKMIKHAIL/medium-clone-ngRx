426fe9447d2ac2322e83dbd35b6d067c1d23e903
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-21 10:41:46 +0100

    ArticleService was covered

--- src/app/shared/services/article.service.spec.ts
+++ src/app/shared/services/article.service.spec.ts
@@ -1,19 +1,42 @@
 import { TestBed } from "@angular/core/testing";
 
 import { ArticleService } from "./article.service";
-import { HttpClientTestingModule } from "@angular/common/http/testing";
+import {
+      HttpClientTestingModule,
+      HttpTestingController,
+} from "@angular/common/http/testing";
+import { environment } from "../../../environments/environment";
 
 describe("FeedService", () => {
       let service: ArticleService;
+      let httpTestingController: HttpTestingController;
 
       beforeEach(() => {
             TestBed.configureTestingModule({
                   imports: [HttpClientTestingModule],
             });
             service = TestBed.inject(ArticleService);
+            httpTestingController = TestBed.get(HttpTestingController);
       });
 
       it("should be created", () => {
             expect(service).toBeTruthy();
+      });
+
+      it("getArticle", () => {
+            service.getArticle("requestDataSlug32").subscribe(resp => {
+                  expect(resp).toBeTruthy();
+                  expect(resp.slug).toBe("requestDataSlug32");
+            });
+            const req = httpTestingController.expectOne(
+                  `${environment.apiUrl}/articles/requestDataSlug32`,
+            );
+            expect(req.request.method).toBe("GET");
+
+            req.flush({
+                  article: {
+                        slug: "requestDataSlug32",
+                  },
+            });
       });
 });
