be636bce1e27edbe0ee8ebb49654b1654bf2b612
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-22 16:24:55 +0100

    cover SettingsComponent

--- src/app/settings/components/settings/settings.component.spec.ts
+++ src/app/settings/components/settings/settings.component.spec.ts
@@ -1,13 +1,22 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import {
+      ComponentFixture,
+      fakeAsync,
+      flush,
+      TestBed,
+      tick,
+} from "@angular/core/testing";
 
 import { SettingsComponent } from "./settings.component";
 import { FormsModule, ReactiveFormsModule } from "@angular/forms";
 import { provideMockStore } from "@ngrx/store/testing";
-import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
+import { of } from "rxjs";
 
 describe("SettingsComponent", () => {
       let component: SettingsComponent;
       let fixture: ComponentFixture<SettingsComponent>;
+      let element: DebugElement;
 
       beforeEach(async () => {
             await TestBed.configureTestingModule({
@@ -20,9 +29,44 @@ describe("SettingsComponent", () => {
             fixture = TestBed.createComponent(SettingsComponent);
             component = fixture.componentInstance;
             fixture.detectChanges();
+            element = fixture.debugElement;
       });
 
       it("should create", () => {
             expect(component).toBeTruthy();
+      });
+
+      it("h1 correct text", () => {
+            const title = element.query(By.css(".text-xs-center"));
+
+            expect(title.nativeElement.textContent).toBe("Your Settings");
+      });
+
+      it("button correct text", () => {
+            component.currentUser = {
+                  bio: "bio string",
+                  email: "email string",
+                  image: "",
+                  token: "token string",
+                  username: "username string",
+            };
+            component.initializeForm();
+            fixture.detectChanges();
+
+            const form = element.query(By.css("form"));
+            fixture.detectChanges();
+
+            const button = form.query(By.css("button"));
+            fixture.detectChanges();
+
+            expect(button.nativeElement.textContent).toBe(" Update Settings ");
+      });
+
+      it("show loading comp", () => {
+            component.isSubmitting$ = of(true);
+            fixture.detectChanges();
+
+            const loadingComp = element.query(By.css("mc-loading"));
+            expect(loadingComp).toBeTruthy();
       });
 });
