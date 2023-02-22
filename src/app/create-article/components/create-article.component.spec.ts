cd3d5a15ffbef3c57b3d59ffb0f2df955f4bbd83
YACHNIKMIKHAIL <limerick2011@yandex.by>
2023-02-22 15:49:18 +0100

    cover ArticleFormComponent

--- src/app/create-article/components/create-article.component.spec.ts
+++ src/app/create-article/components/create-article.component.spec.ts
@@ -2,11 +2,13 @@ import { ComponentFixture, TestBed } from "@angular/core/testing";
 
 import { CreateArticleComponent } from "./create-article.component";
 import { provideMockStore } from "@ngrx/store/testing";
-import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
+import { CUSTOM_ELEMENTS_SCHEMA, DebugElement } from "@angular/core";
+import { By } from "@angular/platform-browser";
 
 describe("CreateArticleComponent", () => {
       let component: CreateArticleComponent;
       let fixture: ComponentFixture<CreateArticleComponent>;
+      let element: DebugElement;
 
       beforeEach(async () => {
             await TestBed.configureTestingModule({
@@ -18,9 +20,19 @@ describe("CreateArticleComponent", () => {
             fixture = TestBed.createComponent(CreateArticleComponent);
             component = fixture.componentInstance;
             fixture.detectChanges();
+            element = fixture.debugElement;
       });
 
       it("should create", () => {
             expect(component).toBeTruthy();
+      });
+
+      it("article-form should submit article", () => {
+            spyOn(component, "createArticle");
+            const formComp = element.query(By.css("mc-article-form"));
+
+            formComp.triggerEventHandler("articleSubmit", {});
+
+            expect(component.createArticle).toHaveBeenCalledTimes(1);
       });
 });
